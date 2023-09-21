de421de9f2594943527451ab411c8bba00821a0e
Kirill Yatsenko <kirillyat@gmail.com>
2023-09-21 17:37:24 +0300

    modified:   hw2/code/4.py

--- hw2/code/4.py
+++ hw2/code/4.py
@@ -13,7 +13,7 @@ class SingleManager:
 
     def close(self, i: int):
         if self.stack:
-            self.stack.pop(-1) + 1
+            self.stack.pop(-1)+1
         else:
             self.min = i
 
@@ -22,11 +22,6 @@ class SingleManager:
             return self.stack[-1]
         return self.min
 
-    def process(self, i: int, c: str):
-        if c in "({[":
-            self.open(i)
-        else:
-            self.close(i)
 
 
 class TripleManager:
@@ -42,14 +37,20 @@ class TripleManager:
         self.managers = [SingleManager(), SingleManager(), SingleManager()]
 
         for i, c in enumerate(s):
-            if c in "()":
-                self.managers[0].process(i, c)
-            if c in "{}":
-                self.managers[1].process(i, c)
-            if c in "[]":
-                self.managers[2].process(i, c)
+            if c == ')':
+                self.managers[0].close(i+1)
+            elif c == '(':
+                self.managers[0].open(i+1)
+            elif c == '}':
+                self.managers[1].close(i+1)
+            elif c == '{':
+                self.managers[1].open(i+1)
+            elif c == ']':
+                self.managers[2].close(i+1)
+            elif c == '[':
+                self.managers[2].open(i+1)
 
-            cur = min([i - m.low() for m in self.managers])
+            cur = min([i+1 - m.low() for m in self.managers])
             if cur > 0 and cur == self.ans:
                 self.n += 1
             if cur > self.ans:
@@ -58,4 +59,4 @@ class TripleManager:
         return self.ans, self.n
 
 
-print(TripleManager().process(input()))
+print(*TripleManager().process(input()))
